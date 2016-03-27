4f1bb829c25e442714e0dcda65590c98b6fa4b8b
mapuchuz <mapuchuz@gmail.com>
2016-03-27 21:23:35 +0200

    JPA actif sur table animal

---
+++ src/main/java/Accueil.java
@@ -0,0 +1,23 @@
+
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Accueil {
+
+//	static private List<Enclos>enclos= 	new ArrayList<>();
+
+	public String enclos() {
+		return "enclos";
+	}
+	public String animaux() {
+		return "animaux";
+	}
+	public String sandbox() {
+		return "sandbox";
+	}
+	public String visites() {
+		return "visites";
+	}
+	
+}
